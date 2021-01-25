fac743b81c75e0d67955437c41a72b272b66886c
EvandroVogel <65269211+EvandroVogel@users.noreply.github.com>
2021-01-25 19:55:29 -0300

    Add files via upload

---
+++ texto.py
@@ -0,0 +1,24 @@
+def nao_primo():
+  lista = [7, 11, 13, 17, 19, 23, 29, 31]
+  segunda_lista = []
+
+  for item in lista:
+    for outro_item in lista:
+      produto = item * outro_item
+      modulo = produto % 30
+      if(modulo == 1):
+        modulo = 30 + modulo
+      divisao = produto // 30
+      tupla = (item, outro_item, produto, divisao, modulo)
+      segunda_lista.append(tupla)
+  return segunda_lista
+
+a = nao_primo()
+
+import pandas as pd
+
+pd.set_option('display.max_rows', 100)
+
+df = pd.DataFrame(a)
+display( df.rename(columns={0: 'Primeiro Valor',1: 'Segundo Valor',2: 'Produto',
+                          3: 'Resto da Divisao', 4: 'Parte Inteira da Divisao'}))
